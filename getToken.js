64598a3479db39d46f85cf770290a0b93ba5a6b6
Phantomlnfinity <168790497+Phantomlnfinity@users.noreply.github.com>
2024-11-02 00:17:16 -0700

    no cache

--- getToken.js
+++ getToken.js
@@ -4,7 +4,7 @@ request.onsuccess = function(e) {
         new TextEncoder("utf-8"), text = Array.from(e.target.result.contents);
         var t = new TextDecoder("utf-8").decode(new Uint8Array(text)),
             o = t.indexOf("eyJ");
-        var newBookmark = 'javascript:var newToken = "' + t.substring(o, o + 172) + '"; fetch("https://raw.githubusercontent.com/Phantomlnfinity/dashcraft-bookmarklets/refs/heads/main/signIn.js", {cache: "no-store"}).then(data=>{data.text().then(text=>{eval(text)})});'
+        var newBookmark = 'javascript:var newToken = "' + t.substring(o, o + 172) + '"; fetch("https://raw.githubusercontent.com/Phantomlnfinity/dashcraft-bookmarklets/refs/heads/main/signIn.js", {cache: "no-cache"}).then(data=>{data.text().then(text=>{eval(text)})});'
         document.write("<html><body><center><h1>Account Token</h1><p>Before you give your account token to anybody, make sure that you completely trust whoever you're giving it to. An account token allows somebody to have full, permanent access to your DashCraft account.<br><br><br>If you still want to get your token, here it is:</p>" + t.substring(o, o + 172) + "<br><br><a class='bookmarklet' href='" + newBookmark + "'>Use this bookmarklet to sign in more easily</a><style>html { height: 100%; width: 100%; color: lightgrey; scrollbar-color: grey black; scrollbar-width: none; font-family:Arial, Helvetica, sans-serif } body { align-content: center; text-align: center; background-color: #1a1a1a; } p { width: min(100%, 500px); } .bookmarklet { background-color: lightgrey; border-radius: 10px; border-width: 0px; padding: 10px; color: black; text-decoration: none; user-select: none; } </style</body></html>"), document.close()
     }
 };
